1efdbc0742ea7eb4524988e42f5ab882ad63a079
João Lucas Ito <joaolucas.itosan@gmail.com>
2023-08-29 08:32:51 -0300

    save

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import App from './pages/home';
+import Contato from './pages/contato';
+import Tarefas from './pages/tarefas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/contato' element={<Contato/>}/>
+        <Route path='/tarefas' element={<Tarefas/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
--- src/pages/tarefas/index.js
+++ src/pages/tarefas/index.js
@@ -1,32 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import './index.scss';
 
 
 export default function Tarefas() {
+  const [tarefa, setTarefa] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+
+  function alterarTarefa(e) {
+    setTarefa(e.target.value);
+  }
+
+  function adicionar() {
+    tarefas.push(tarefa);
+    setTarefas([...tarefas]);
+    
+    setTarefa('');
+  }
+
 
   return (
     <div className="pagina-lista-tarefa">
       <div className='container'>
         <h1> Lista de Tarefas </h1>
         <div>
-          Adicionar tarefa: <input type='text' />
-          <button> Adicionar </button>
+          Adicionar tarefa: <input type='text' value={tarefa} onChange={alterarTarefa} />
+          <button onClick={adicionar}> Adicionar </button>
         </div>
         <div>
           <ul>
-            <li> Item 1 </li>
-            <li> Item 2 </li>
-            <li> Item 3 </li>
-            <li> Item 2 </li>
-            <li> Item 3 </li>
-            <li> Item 1 </li>
-            <li> Item 2 </li>
-            <li> Item 3 </li> 
+            {tarefas.map(item => <li> {item} </li>)}
           </ul>
+          
         </div>
       </div>
-
-      <Link className='voltar' to='/'> Voltar para Menu </Link>
     </div>
   )
 }
