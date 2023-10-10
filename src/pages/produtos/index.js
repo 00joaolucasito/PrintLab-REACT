08ae3635068e70b07c1b3e3f2874832e54093089
João Lucas Ito <joaolucas.itosan@gmail.com>
2023-10-10 08:24:45 -0300

    save

---
+++ src/pages/produtos/index.js
@@ -0,0 +1,22 @@
+import './index.scss';
+
+
+export default function Produtos(){
+
+    return(
+    <div className='pagina-produtos'>
+        <div className='main'>
+             <div className='lado'>
+                <h1>Qualquer categoria</h1>
+                <p>Impressoras 3d</p>
+                <p>Filamentos</p>
+                <p>Resina</p>
+                <p>Impressões</p>
+            </div>
+            <div className='meio'> 
+
+            </div>
+        </div>
+    </div>
+    );
+}
