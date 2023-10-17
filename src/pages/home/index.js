5abf72805c2d324ccebbcc608a0c0c74621a12b8
João Lucas Ito <joaolucas.itosan@gmail.com>
2023-10-17 09:16:59 -0300

    save

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
 import Cabecalho from '../../components/cabecalho/Cabecalho';
-import DivHome from '../../components/divHome';
 import Creality from '../../components/creality';
 import Rodape from '../../components/rodape';
 
@@ -9,27 +8,91 @@ import Rodape from '../../components/rodape';
 export default function Home() {
   return (
     <div className='home'>
+      <Cabecalho />
+      <section class="ppa">
+
+        <div id="imagemfundo">
+
+        </div>
+      </section>
+      <section class="tpo">
+        <div id="tp1">
+          <div id="imgtp" >
+
+          </div>
+          <div id="tp2">
+            <h1>
+              Impressoras 3D
+            </h1>
+
+            <p>
+              Dê vida às suas ideias com precisão e criatividade. Impressoras 3D de última geração, disponíveis agora para transformar sua imaginação em realidade.
+            </p>
+
+            <button>
+              Saiba mais
+            </button>
+          </div>
 
 
-            <Cabecalho/>
-            <div className='fundo'></div>
-            <div className='home'>
-            <DivHome pos="pos1" img='/assets/images/l1.png' title='Impressoras 3D' texto='Dê vida às suas ideias com precisão e criatividade. Impressoras 3D de última geração, disponíveis agora para transformar sua imaginação em realidade.' />
-            <DivHome pos="pos2" img='/assets/images/l2.png' title='Filamentos' texto='Desperte sua criatividade tridimensional com o filamento perfeito para suas impressões 3D. Qualidade e precisão que transformam ideias em realidade.' />
-            <DivHome pos="pos1" img='/assets/images/l3.png' title='Impressões' texto='Descubra a magia da impressão tridimensional e adquira peças exclusivas que vão encantar e surpreender você. Utilizamos filamentos sustentáveis feitos de materiais reciclados.' />
-            </div>
-
-        <div className='cont2'>
-          <div> <h1>O que são Impressoras 3D?</h1> </div>
-          <div> <p>Uma impressora 3D é uma máquina revolucionária que permite criar objetos tridimensionais a partir de um modelo digital. Ela utiliza uma variedade de materiais, como plástico, resina, metal ou até mesmo alimentos, para construir camadas sucessivas e criar a forma desejada. Esses materiais são aquecidos e depositados em camadas finas, seguindo as especificações do modelo digital, até que o objeto seja completamente formado. Com a impressora 3D, é possível materializar praticamente qualquer coisa, desde pequenos acessórios até peças complexas e funcionais, abrindo um mundo de possibilidades criativas e práticas.</p> </div>
-          <div className='duvida'> <p>Alguma dúvida? Nos contate enviando um e-mail !</p> </div>
         </div>
 
-        <Creality/>
+        <div id="tp1">
 
-        <Rodape/>
+          <div id="tp2">
+            <h1 class="ppw">
+              Filamentos
+            </h1>
 
-      </div>
+            <p id="ppas">
+              Desperte sua criatividade tridimensional com o filamento perfeito para suas impressões 3D. Qualidade e precisão que transformam ideias em realidade.
+            </p>
+
+            <button class="asps">
+              Saiba mais
+            </button>
+          </div>
+          <div class="imgpd"></div>
+        </div>
+
+
+        <div id="imgpp">
+
+        </div>
+
+        <div id="tp1">
+          <div
+            id="imgop"></div>
+          <div id="tp2">
+            <h1>
+              Impressoras 3D
+            </h1>
+
+            <p>
+              Dê vida às suas ideias com precisão e criatividade. Impressoras 3D de última geração, disponíveis agora para transformar sua imaginação em realidade.
+            </p>
+
+            <button>
+              Saiba mais
+            </button>
+          </div>
+        </div>
+
+        <section id="imgimpre">
+          <h1> O que são impressoras 3D?</h1>
+
+          <p>Uma impressora 3D é uma máquina revolucionária que permite criar objetos tridimensionais a partir de um modelo digital. Ela utiliza uma variedade de materiais, como plástico, resina, metal ou até mesmo alimentos, para construir camadas sucessivas e criar a forma desejada. Esses materiais são aquecidos e depositados em camadas finas, seguindo as especificações do modelo digital, até que o objeto seja completamente formado. Com a impressora 3D, é possível materializar praticamente qualquer coisa, desde pequenos acessórios até peças complexas e funcionais, abrindo um mundo de possibilidades criativas e práticas.</p>
+
+          <div id="classbb">
+            <p>Alguma dúvida? Nos contate enviando um <a href="">e-mail </a>!</p>
+          </div>
+
+
+        </section>
+            <Creality/>
+          <Rodape/>
+        </section>
+    </div>
   );
 }
 
--- src/pages/produtos/index.js
+++ src/pages/produtos/index.js
@@ -1,12 +1,14 @@
 import './index.scss';
 import Cabecalho from '../../components/cabecalho/Cabecalho';
 import Rodape from '../../components/rodape';
+import Opcao from '../../components/opcao';
 
 export default function Produtos(){
 
     return(
     <div className='pagina-produtos'>
         <Cabecalho/>
+        <Opcao/>
         <div className='main'>
              <div className='lado'>
                 <h1>Qualquer categoria</h1>
@@ -18,8 +20,8 @@ export default function Produtos(){
             <div className='meio'> 
 
             </div>
-            <Rodape/>
         </div>
+        <Rodape/>
     </div>
     );
 }
